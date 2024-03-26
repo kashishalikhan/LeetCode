986e67ed3c8f28fdd8b4ca76d259242c3bbee9a3
Kashish_Ali_Khan <ar1209shi@gmail.com>
2024-03-26 22:35:09 +0530

    Time: 38 ms (83.67%), Space: 43.4 MB (95.93%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+      int n = nums.size();
+        for (int
+             i = 0; i < n;) {
+            if (nums[i] <= 0 || nums[i] > n || nums[i] == i + 1 || nums[nums[i] - 1] == nums[i]) {
+                i++;
+            } else {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        
+        // Step 2: Find the first missing positive integer
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        return n + 1;
+        
+    }
+};
