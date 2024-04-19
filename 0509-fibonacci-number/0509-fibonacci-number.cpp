241fe173ede2841791287d5d499f37d047bdbdb6
Kashish_Ali_Khan <ar1209shi@gmail.com>
2024-04-19 21:42:01 +0530

    Time: 10 ms (51.38%), Space: 6.9 MB (95.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 1 || n == 0)
+        if(n == 0 || n == 1)
             return n;
-        else
-            return fib(n - 1) + fib(n -2);
+        int ans = fib(n - 1) + fib(n - 2);
+        return ans;
     }
 };
